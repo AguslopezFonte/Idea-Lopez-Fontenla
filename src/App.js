2f40c708ed5bb828d173d1e09af04c7bf0decea2
agusfonte <agustin.cuttone@gmail.com>
2022-06-29 21:10:10 -0300

    Tp5

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/NavBar';
 import Body from './components/Body';
-import Welcome from './components/Welcome';
+import Card from './components/Card';
 import Contador from './components/Contador';
 
 function App() {
   return (
 
     <div>
-      <Contador/>
+      {/*<Contador stock={5}/>*/}
       <Navbar/>
-      <Welcome name ="Agustin"/>
-      <Body>
-        <Card/>
-      </Body>
+      <Body/> 
     </div>
   );
 }
--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,14 +1,37 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Swal from 'sweetalert2';
 
-function Contador() {
+
+function Contador({stock}) {
     const [num, setNum] = useState(0);
 
+    useEffect(()=> {
+        if(num == stock -2){
+            Swal.fire({
+                tittle: 'Atencion!',
+                text: 'Quedan pocas unidades!',
+                icon: 'warning',
+                timer: 2000
+            })
+        }
+    }, [num])
+
     const sumar = () => { 
+        if(num<stock) {
         setNum (num + 1)
     }
-
+    else{
+        Swal.fire({
+            title: 'Atencion!',
+            text: 'Ya ha llegado al stock maximo de este producto',
+            icon: 'warning',
+            timer: 2000
+        })
+    }
+}
     const resta = () => { 
+        if(num>0){
         setNum (num - 1)
     }
     const reiniciar = () => { 
@@ -17,13 +40,15 @@ function Contador() {
     
   return (
       <>
-      <p>{num}</p>
+      <h3 style= {{marginLeft:20}}>{num}</h3>
+      <p>{stock > 1? `${stock} unidades disponibles` : `${stock} unidad dispobible` }</p>
       <button onClick={sumar}> Suma!</button>
       <button onClick={resta}> Resta!</button>
       <button onClick={reiniciar}> Reiniciar!</button>
+
+      <button> {num > 0 ? `Agregar al carrito` : `Elegi tu cantidad`}</button>
       </>
-     
   )
 }
 
-export default Contador;
+export default Contador }
