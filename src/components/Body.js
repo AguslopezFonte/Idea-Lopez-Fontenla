6fce6918e9995e98bd7a2c1967cbf83b7bc8ba96
agusfonte <agustin.cuttone@gmail.com>
2022-07-04 15:46:41 -0300

    commit para Jose

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,32 @@
 
-import { Children } from 'react';
+import { useEffect, useState } from 'react';
 import './Body.css';
-import Card from './Card';
+import Card from './Card'
 
-function Body({}) {
+function Body() {
+
+  const[info, setInfo] = useState ([])
+
+
+  useEffect(()=>{
+    fetch('data.json')
+    .then((resp) => resp.json())
+    .then((data)=> setInfo(data))
+  
+
+  }, [])
+
   return (
      <section className='cuerpo'>
+       <div>
        <h2> Catalogo de Productos</h2>
        <p> Elegi tus productos de esta lista</p>
-         <Card product='Mesa' price= '$1000' img=''/>
-         <Card product='Silla' price= '$200'/>
-         <Card product='' price= ''/>
-         <Card product='' price= ''/>
+       </div>
+       <div className='catalogo'>
+         {info.map(i => <Card product ={i.nombre} price={i.precio} />)}
+         </div>
+       
+      
      </section>
 
   );
